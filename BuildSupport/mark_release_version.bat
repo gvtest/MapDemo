b6b0db5ca5a58913d8c74d6b4f08a2d461b1fb3d
gvtest <gvtest@bobmail.info>
2013-11-29 12:11:55 +0000

    Build scripts

--- BuildSupport/mark_release_version.bat
+++ BuildSupport/mark_release_version.bat
@@ -12,4 +12,4 @@ REM Replaces "[assembly: AssemblyVersion("1.0.1.0")]" with "[assembly: AssemblyV
 "C:\Program Files (x86)\Jenkins\fnr.exe" --cl --dir "%WORKSPACE%\GMap.NET.Core\Properties" --fileMask "GlobalAssemblyInfo.cs" --caseSensitive --useRegEx --find "\[assembly: AssemblyVersion\(\"(\d+\.\d+\.\d+)\.0\"\)\]" --replace "[assembly: AssemblyVersion(\"$1.%BUILD_NUMBER%\")]"
     
 REM Replaces "[assembly: AssemblyInformationalVersion("1.2.3 Development Snapshot")]" with "[assembly: AssemblyInformationalVersion("1.2.3 Released Version")]"
-"C:\Program Files (x86)\Jenkins\fnr.exe" --cl --dir "%WORKSPACE%\GMap.NET.Core\Properties" --fileMask "GlobalAssemblyInfo.cs" --caseSensitive --useRegEx --find "\[assembly: AssemblyInformationalVersion\(\"(\d+(\.\d+)+)\s*([^\\"]*)\"\)\]" --replace "[assembly: AssemblyInformationalVersion(\"$1 Released Version\")]" 
+"C:\Program Files (x86)\Jenkins\fnr.exe" --cl --dir "%WORKSPACE%\GMap.NET.Core\Properties" --fileMask "GlobalAssemblyInfo.cs" --caseSensitive --useRegEx --find "\[assembly: AssemblyInformationalVersion\(\"(\d+(\.\d+)+)([^\"]*)\"\)\]" --replace "[assembly: AssemblyInformationalVersion(\"$1 Released Version\")]" 
