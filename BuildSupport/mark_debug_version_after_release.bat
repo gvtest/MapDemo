1e24aebdf6c3d85eaeff7278331452f4d2bd37a5
gvtest <gvtest@bobmail.info>
2013-12-04 08:35:31 +0000

    Build dir changed

--- BuildSupport/mark_debug_version_after_release.bat
+++ BuildSupport/mark_debug_version_after_release.bat
@@ -12,3 +12,4 @@ REM Replaces "[assembly: AssemblyVersion("1.0.1.$BUILD_NUMBER")]" with "[assembl
 
 REM Replaces "[assembly: AssemblyInformationalVersion("1.2.3 Development Snapshot")]" with "[assembly: AssemblyInformationalVersion("1.2.3 Released Version")]"
 "C:\Program Files (x86)\Jenkins\fnr.exe" --cl --dir "%WORKSPACE%\GMap.NET.Core\Properties" --fileMask "GlobalAssemblyInfo.cs" --caseSensitive --useRegEx --find "\[assembly: AssemblyInformationalVersion\(\"(\d+(\.\d+)+)(.*?)\"\)\]" --replace "[assembly: AssemblyInformationalVersion(\"$1 Debug Snapshot\")]" 
+
